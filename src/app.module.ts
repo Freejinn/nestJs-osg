e7b19b505c97004018b521b18bbd5e678ff859a9
Tony Gambone <tonygambone@gmail.com>
2023-12-17 10:30:49 -0700

    Example of a migration that creates data, with supporting changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,25 +2,15 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './entities/user.entity';
 import { ConfigModule } from '@nestjs/config';
-import { SkillParent} from './entities/parentSkill.entity';
-import { SkillBadge } from './entities/skillBadge.entity';
-import { SkillPart } from './entities/skillPart.entity';
+import { dataSourceOptions } from './data-source';
 
 @Module({
-  imports: [AuthModule, UserModule, 
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'Poppy!79',
-      database: 'testosg',
-      entities: [User, SkillParent, SkillBadge, SkillPart],
-      synchronize: true,
-      autoLoadEntities: true,
-  }),
-  ConfigModule.forRoot() ],
+  imports: [
+    AuthModule,
+    UserModule,
+    TypeOrmModule.forRoot(dataSourceOptions),
+    ConfigModule.forRoot(),
+  ],
 })
 export class AppModule {}
---
+++ src/data-source.ts
@@ -0,0 +1,14 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'testosg',
+  password: 'Poppy!79',
+  database: 'testosg',
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  migrations: ['dist/migrations/*.js'],
+  synchronize: true,
+};
+const dataSource = new DataSource(dataSourceOptions);
+export default dataSource;
